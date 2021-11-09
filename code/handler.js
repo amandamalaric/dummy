f0e0e220dab03838530412235cb033070236cb1e
Jacob Craffey <jcraffey@ostusa.com>
2021-11-09 11:48:00 -0500

    actually updated

--- code/handler.js
+++ code/handler.js
@@ -1,29 +1,30 @@
 /* LIBRARIES */
-const aws = require( 'aws-sdk' );
-const dynamo = new aws.DynamoDB.DocumentClient();
+const aws = require("aws-sdk");
+const dynamo = new aws.DynamoDB({ region: "us-east-1" });
 
-async function lookupSKU( macAddress, dynamoDB = null ){
-    var params = {
-        TableName: 'ProvisioningResourcesServerless',
-        Key: {
-            'MacAddress': {S: macAddress}
-        }
-    };
+async function lookupSKU(macAddress, dynamoDB = null) {
+  var params = {
+    "TableName": "ProvisioningResourcesServerless",
+    "Key": {
+      "MacAddress": { "S": macAddress },
+    },
+  };
 
-    const response = await dynamo.getItem(params);
-    return response.Items[0].Sku;
+  const response = await dynamo.getItem(params).promise();
+  return response.Item.SKU;
 }
 
-async function getSKU( event ){
-    const macAddress = event.parameters && event.parameters.MACAddress ? event.parameters.MACAddress : null;
-    const sku = await lookupSKU(macAddress);
+async function getSKU(event) {
+  console.log("Processing Request");
+  const macAddress = event.MacAddress ? event.MacAddress : null;
+  const sku = await lookupSKU(macAddress);
 
-    return {
-        "allowProvisioning": true,
-        "parameterOverrides" : {
-            "SKU": sku,
-        }
-    };
-}c
+  return {
+    allowProvisioning: true,
+    parameterOverrides: {
+      SKU: sku,
+    },
+  };
+}
 
 module.exports.getSKU = getSKU;
